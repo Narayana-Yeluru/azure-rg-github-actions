f07b34d1c2fa450e12611d4a66c5b441e8b0c4d2
Narayana Yeluru <37355660+Narayana-Yeluru@users.noreply.github.com>
2026-03-22 22:19:43 -0400

    Update create-rg.sh

--- scripts/create-rg.sh
+++ scripts/create-rg.sh
@@ -1,14 +1,12 @@
-
 #!/bin/bash
 
-# Variables (edit as needed)
-RESOURCE_GROUP="my-github-rg"
-LOCATION="eastus"
+RESOURCE_GROUP=$1
+LOCATION=$2
 
 echo "Creating resource group: $RESOURCE_GROUP in $LOCATION"
 
 az group create \
-  --name $RESOURCE_GROUP \
-  --location $LOCATION
+  --name "$RESOURCE_GROUP" \
+  --location "$LOCATION"
 
 echo "Resource group created successfully!"
